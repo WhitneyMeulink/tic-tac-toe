70f9df1b0e10849cded1e98f06817b6bab795e24
WhitneyMeulink <whitney.k.meulink@gmail.com>
2024-02-24 19:40:57 -0800

    Tic-Tac-Toe update

---
+++ TicTacToeAOP1/src/PlayTicTacToe.java
@@ -0,0 +1,23 @@
+package TicTacToeAOP1;
+import java.util.Scanner;
+
+public class PlayTicTacToe {
+
+	public static void main(String[] args) {
+	
+		Scanner in = new Scanner(System.in);
+		System.out.print("Player 1, what is your name? ");
+		String p1 = in.nextLine();
+	
+		
+		
+		System.out.print("Player 2, what is your name? ");
+		String p2 = in.nextLine();
+
+
+	
+	
+		
+	}
+
+}
